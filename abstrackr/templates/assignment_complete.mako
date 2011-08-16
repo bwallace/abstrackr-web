e2435800723b9bebc9fd9859e8a1699c13dbe127
byron.wallace <byron.wallace@gmail.com>
2011-08-16 17:09:45 -0400

    git funkiness

--- abstrackr/templates/assignment_complete.mako
+++
@@ -1,4 +0,0 @@
-
-<h2>Huzzah! You've completed this assignment.</h2> <br/></br><h2> Nice work.</h2>
-
-
--- abstrackr/tests/functional/test_account.py
+++
@@ -1,7 +0,0 @@
-from abstrackr.tests import *
-
-class TestAccountController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='account', action='index'))
-        # Test response...
