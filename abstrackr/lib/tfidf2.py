a0f75712897bf1fd7784cadf6bfec46206513c7c
Jens Jap <jensjap@gmail.com>
2013-08-22 13:25:33 -0400

    stop_list.txt is no longer being used. We just hardcoded the list into the source

--- abstrackr/lib/tfidf2.py
+++ abstrackr/lib/tfidf2.py
@@ -87,21 +87,22 @@ except:
 import copy
 
 
-stop_list_path=os.getcwd() + "/abstrackr/lib/stop_list.txt"
-#stop_list_path = "/Users/abstrackr-user/Hive/abstrackr/abstrackr/lib/stop_list.txt"
+#stop_list_path=os.getcwd() + "/abstrackr/lib/stop_list.txt"
+stop_list_path = "/Users/abstrackr-user/Hive/abstrackr/abstrackr/lib/stop_list.txt"
 
 print "stop word list path is %s" % stop_list_path
 
 def build_stop_list(stop_list_path):
-    exclude_words = []
-    if stop_list_path != None:
-        f = open(stop_list_path)
-        while 1:
-            line = f.readline()
-            if not line:
-                break
-            # Every line is assumed to be a single word
-            exclude_words.append(line.strip())
+    exclude_words = [a, about, above, across, after, afterwards, again, against, all, almost, alone, along, already, also, although, always, am, among, amongst, amoungst, amount, an, and, another, any, anyhow, anyone, anything, anyway, anywhere, are, around, as, at, back, be, became, because, become, becomes, becoming, been, before, beforehand, behind, being, below, beside, besides, between, beyond, bill, both, bottom, but, by, call, can, cannot, cant, co, con, could, couldnt, cry, de, describe, detail, do, done, down, due, during, each, eg, eight, either, eleven, else, elsewhere, empty, enough, etc, even, ever, every, everyone, everything, everywhere, except, few, fifteen, fify, fill, find, fire, first, five, for, former, formerly, forty, found, four, from, front, full, further, get, give, go, had, has, hasnt, have, he, hence, her, here, hereafter, hereby, herein, hereupon, hers, herself, him, himself, his, how, however, hundred, i, ie, if, in, inc, indeed, interest, into, is, it, its, itself, keep, last, latter, latterly, least, less, ltd, made, many, may, me, meanwhile, might, mill, mine, more, moreover, most, mostly, move, much, must, my, myself, name, namely, neither, never, nevertheless, next, nine, no, nobody, none, noone, nor, not, nothing, now, nowhere, of, off, often, on, once, one, only, onto, or, other, others, otherwise, our, ours, ourselves, out, over, own, part, per, perhaps, please, put, rather, re, same, see, seem, seemed, seeming, seems, serious, several, she, should, show, side, since, sincere, six, sixty, so, some, somehow, someone, something, sometime, sometimes, somewhere, still, such, system, take, ten, than, that, the, their, them, themselves, then, thence, there, thereafter, thereby, therefore, therein, thereupon, these, they, thick, thin, third, this, those, though, three, through, throughout, thru, thus, to, together, too, top, toward, towards, twelve, twenty, two, un, under, until, up, upon, us, very, via, was, we, well, were, what, whatever, when, whence, whenever, where, whereafter, whereas, whereby, wherein, whereupon, wherever, whether, which, while, whither, who, whoever, whole, whom, whose, why, will, with, within, without, would, yet, you, your, yours, yourself, yourselves]
+#    exclude_words = []
+#    if stop_list_path != None:
+#        f = open(stop_list_path)
+#        while 1:
+#            line = f.readline()
+#            if not line:
+#                break
+#            # Every line is assumed to be a single word
+#            exclude_words.append(line.strip())
     return exclude_words
 
 # if you don't want to use a stop list, just set the stop_list global
