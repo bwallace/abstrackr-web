f2665ff22b3e57eafb4c1f873af292c575f4e9f3
Jens Jap <jensjap@gmail.com>
2014-08-20 16:27:47 -0500

    Ensures order when saving authors.

--- abstrackr/lib/xml_to_sql.py
+++ abstrackr/lib/xml_to_sql.py
@@ -9,6 +9,7 @@ import string
 import csv
 import random
 import os
+import collections
 
 # third party
 import sqlite3
@@ -154,7 +155,7 @@ def ris_to_d(ris_data):
                 cur_id = internal_id
 
         elif field in ("ER"):
-            current_citation["authors"] = list(set(cur_authors))
+            current_citation["authors"] = list(OrderedSet(cur_authors))
             current_citation["keywords"] = list(cur_keywords)
             ris_d[cur_id] = current_citation
     
@@ -408,6 +409,67 @@ def xml_to_dict(fpath):
     print "\nFinished. Returning %s title/abstract/keyword sets, %s of which have no abstracts.\n" \
                     % (len(ref_ids_to_abs.keys()), num_no_abs)
     return ref_ids_to_abs, {"import-errors": parsing_errors}
+
+
+class OrderedSet(collections.MutableSet):
+""" Stolen from http://code.activestate.com/recipes/576694/
+"""
+
+    def __init__(self, iterable=None):
+        self.end = end = []
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:
+            key, prev, next = self.map.pop(key)
+            prev[2] = next
+            next[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = self.end[1][0] if last else self.end[2][0]
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
 
 
 
